4b9dfa42ff1eb983480b4c1c10a0e19651ea6807
Puneeth-77 <puneethvishwanath89@gmail.com>
2026-09-22 14:19:57 +0530

    Refactor input prompts for clarity in app.py

--- Puneeth/app.py
+++ Puneeth/app.py
@@ -1,6 +1,6 @@
 import streamlit as st
-a=st.number_input("Enter a num:")
-b=st.number_input("Enter another num:")
+a=st.number_input("Enter num 1:")
+b=st.number_input("Enter num 2:")
 if st.button("Add"):
 	st.success(a+b)
 elif st.button("Sub"):
